e79c99a6c35b3bd806d74cf3bfb9f24f2068d5d5
Xunze <tanxunze@outlook.com>
2024-11-22 18:42:44 +0000

    add a special

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -11,6 +11,14 @@ import { Typography } from "@mui/material";
 import React from "react";
 
 function CustomTimeline({ events }) {
+
+    const shouldReplaceTitleWithImage = (title) => {
+      return title === "Arcaea ptt reached 13.00"; // 替换成你想匹配的标题
+    };
+
+    const replacementImageUrl =
+      "https://cfr2.mionet.top/mionet-a/2024/11/23/6740cf58af622.png";
+
   return (
     <Timeline position="alternate">
       {events.map((event, index) => (
@@ -23,9 +31,21 @@ function CustomTimeline({ events }) {
             {index < events.length - 1 && <TimelineConnector />}
           </TimelineSeparator>
           <TimelineContent>
-            <Typography variant="h6" component="span">
-              {event.title}
-            </Typography>
+            {shouldReplaceTitleWithImage(event.title) ? (
+              <img
+                src={replacementImageUrl}
+                alt="3 star"
+                style={{
+                  width: "100%",
+                  maxHeight: "100px",
+                  objectFit: "contain",
+                }}
+              />
+            ) : (
+              <Typography variant="h6" component="span">
+                {event.title}
+              </Typography>
+            )}
             <Typography>{event.description}</Typography>
           </TimelineContent>
         </TimelineItem>
